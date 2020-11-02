e4d298fb88af6566c5064c1aff2bb376f9749576
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2020-11-02 16:46:50 +0100

    MediaEditor v2

--- src/stubs/mediatypes/MediaEditor.php
+++ src/stubs/mediatypes/MediaEditor.php
@@ -6,16 +6,24 @@ class MediaEditor {
 
 	public function resizeAndSaveImage($file, $filename, $directory)
 	{
-        $img = \Image::make( $file->getRealPath() );
+		$img = \Image::make( $file->getRealPath() );
         $img->backup();
 
-        $img->fit(600, 200);
-        $img->save( storage_path('app/public/'.$directory.'/full/').$filename );
-
+        $img->fit(250, 150);
+        $img->save( storage_path('app/public/'.$directory.'/250x150/').$filename );
         $img->reset();
 
-        $img->fit(250, 150);
-        $img->save( storage_path('app/public/'.$directory.'/display/').$filename );
+        $img->fit(350, 150);
+        $img->save( storage_path('app/public/'.$directory.'/350x150/').$filename );
+		$img->reset();
+
+		$img->fit(600, 200);
+        $img->save( storage_path('app/public/'.$directory.'/600x200/').$filename );
+		$img->reset();
+
+		$img->fit(110, 110);
+        $img->save( storage_path('app/public/'.$directory.'/thumb/').$filename );
+		$img->reset();
 
         return true;
 	}
