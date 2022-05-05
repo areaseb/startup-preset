29bde853b5b46ac9862f91e3fdc6c5293a98d84b
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2022-05-05 16:50:01 +0200

    Changed PresetCommand to UiCommand

--- src/StartupServiceProvider.php
+++ src/StartupServiceProvider.php
@@ -4,12 +4,13 @@ namespace Areaseb\StartupPreset;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
+use Laravel\Ui\UiCommand;
 
 class StartupServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        PresetCommand::macro('startup', function($command){
+        UiCommand::macro('startup', function($command){
             Preset::install();
             $command->info('All done. Make sure to require the others packages and compile your assets');
         });
