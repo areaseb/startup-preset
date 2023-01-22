b0c9460ed727651d0d8fda2a0d3c6738573aa148
DavideMoscheni <108459623+DavideMoscheni@users.noreply.github.com>
2023-01-22 18:27:51 +0100

    Test for L9

--- src/StartupServiceProvider.php
+++ src/StartupServiceProvider.php
@@ -11,7 +11,7 @@ class StartupServiceProvider extends ServiceProvider
     public function boot()
     {
         UiCommand::macro('startup', function($command){
-            Preset::install();
+            // Preset::install();
             $command->info('All done. Make sure to require the others packages and compile your assets');
         });
     }
