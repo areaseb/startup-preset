d1ada7aece18dbe62743dec43e60a2f778f8e7e3
davide.moscheni@areaseb.it <davide.moscheni@areaseb.it>
2024-03-01 18:00:16 +0100

    fix register lead

--- src/stubs/routes/web.php
+++ src/stubs/routes/web.php
@@ -22,38 +22,125 @@ Route::get('tracker', [ReportController::class, 'tracker']);
 Route::get('unsubscribe', [ReportController::class, 'unsubscribe']);
 Route::get('track', [ReportController::class, 'track']);
 
-use Areaseb\Core\Models\{Client, Contact, NewsletterList, Setting};
+use Areaseb\Core\Models\{Client, Contact, Company, NewsletterList, Setting, Note};
 Route::get('register-lead', function(){
 
     $url = 'https://www.'.Setting::base()->sitoweb;
-    $urlPP = 'naturalmenteprimi';
-
-    $ref = request()->headers->get('referer');
-    if($ref == '')
-    {
-        $ref = request('referer');
+    $email = strtolower(request('email'));
+    $nome = ucfirst(strtolower(request('nome')));
+    $cognome = ucfirst(strtolower(request('cognome')));
+    if(request('message') != ''){
+    	$nota = trim(request('message'));
+    } elseif(request('your-message') != ''){
+    	$nota = trim(request('your-message'));    	
     }
+    
 
-    $ref = request()->headers->get('referer');
-    if((strpos($ref, $url) !== false) || (strpos($ref, $urlPP) !== false))
+    if(!Contact::where('email', request('email'))->exists())
     {
-        if(!Contact::where('email', request('email'))->exists())
-        {
-            $contact = new Contact;
-                $contact->email = request('email');
-                $contact->nome = request('nome');
-                $contact->cognome = request('cognome');
-                $contact->cellulare = request('telefono');
-                $contact->subscribed = intval(request('newsletter'));
-                $contact->origin = "Sito";
-            $contact->save();
-            $contact->clients()->attach(Client::Lead());
+        $company = new Company;
+            $company->email = $email;
+            $company->rag_soc = $nome . ' ' . $cognome;
+            $company->mobile = request('telefono');
+            $company->private = true;
+            $company->origin = "Sito";
+            $company->client_id = Client::Lead()->id;
+        $company->save();
 
-            if(intval(request('newsletter')))
+        $contact = new Contact;
+            $contact->email = $email;
+            $contact->nome = $nome;
+            $contact->cognome = $cognome;
+            $contact->cellulare = request('telefono');
+            $contact->subscribed = intval(request('newsletter'));
+            $contact->origin = "Sito";
+            $contact->company_id = $company->id;
+        $contact->save();
+
+
+        if(request('message'))
+        {
+            Note::create([
+                'company_id' => $company->id,
+                'description' => $nota
+            ]);
+        }
+
+        if(request('your-message'))
+        {
+            Note::create([
+                'company_id' => $company->id,
+                'description' => $nota
+            ]);
+        }
+
+
+        if(intval(request('newsletter')))
+        {
+            $list = NewsletterList::firstOrCreate(['nome' => 'Contatti da sito']);
+            $contact->lists()->attach($list->id);
+        }
+    }
+    else
+    {
+        $company = Company::where('email', $email)->first();
+
+        if($company)
+        {
+            if(request('message'))
             {
-                $list = NewsletterList::firstOrCreate(['nome' => 'Contatti da sito', 'owner_id' => 1]);
-                $contact->lists()->attach($list->id);
+                Note::create([
+                    'company_id' => $company->id,
+                    'description' => $nota
+                ]);
             }
+
+            if(request('your-message'))
+            {
+                Note::create([
+                    'company_id' => $company->id,
+                    'description' => $nota
+                ]);
+            }
+
+        }
+        else
+        {
+            $contact = Contact::where('email', $email)->first();
+            $company = new Company;
+                $company->email = $email;
+                $company->rag_soc = $contact->nome . ' ' . $contact->cognome;
+                $company->mobile = $contact->cellulare;
+                $company->private = true;
+                $company->address = $contact->indirizzo;
+                $company->origin = "Sito";
+                $company->client_id = Client::Lead()->id;
+                $company->zip = $contact->cap;
+                $company->nation = $contact->nazione;
+                $company->lang = $contact->lingua;
+                $company->province = $contact->provincia;
+                $company->city = $contact->citta;
+            $company->save();
+
+            $contact->update(['company_id' => $company->id]);
+
+
+            if(request('message'))
+            {
+                Note::create([
+                    'company_id' => $company->id,
+                    'description' => $nota
+                ]);
+            }
+
+            if(request('your-message'))
+            {
+                Note::create([
+                    'company_id' => $company->id,
+                    'description' => $nota
+                ]);
+            }
+
         }
     }
     return redirect($url.'/grazie');
